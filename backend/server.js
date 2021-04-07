91a82e668d0b24e3a35d4b4e2655a828b9646e98
Paras Garg <paras.garg@xebia.com>
2021-04-07 13:34:10 +0530

    initial

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,8 @@
 const express = require('express')
+const student = require('./routes/api/student');
+const student = require('./routes/api/faculty');
+const student = require('./routes/api/thirdParty');
+
+const app = express();
+
+app.use('/api/students', students)
